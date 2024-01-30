6ab5240907364810dbd3c3a917d566a395eb8be7
mikkoplus <sharov.012@gmail.com>
2024-01-30 15:26:44 +0300

    feat(comp): update app component, delete menu component, import footer

--- src/Components/App/ui/App.tsx
+++ src/Components/App/ui/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import style from './App.module.css';
 import Header from '@component/Header';
-import Menu from '@component/Menu';
+// import Menu from '@component/Menu';
 import { useMenuState } from '@/hooks/useMenuState';
 import Popup from '@/Components/popup';
 import { useModalState } from '@/hooks/useModalState';
 import Promo from '@/Components/Promo';
 import FAQ from '@/Components/FAQ';
 import CheckUps from '@/Components/CheckUps';
+import Footer from '@/Components/Footer';
 
-export const App = () => {
+export const App: React.FC = () => {
   const { isActive, switchActiveState } = useMenuState();
   const { closeModal, isModalVisible, openModal } = useModalState();
 
@@ -20,11 +21,11 @@ export const App = () => {
         isMenuActive={isActive}
         openPopup={openModal}
       />
-      <div className={style.invisibleArea}></div>
-      <Menu isActive={isActive} openPopup={openModal} />
+
       <Promo />
       <FAQ />
       <CheckUps />
+      <Footer />
       <Popup closeModal={closeModal} isModalOpen={isModalVisible} />
     </div>
   );
