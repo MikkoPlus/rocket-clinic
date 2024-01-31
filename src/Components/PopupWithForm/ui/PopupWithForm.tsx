0a64d5c5f8601c5c763c02f41f338bf06792b143
mikkoplus <sharov.012@gmail.com>
2024-01-31 16:44:11 +0300

    refactor(comp): refactor popup with form component separate and logic from presentation

--- src/Components/PopupWithForm/ui/PopupWithForm.tsx
+++ src/Components/PopupWithForm/ui/PopupWithForm.tsx
@@ -1,79 +1,84 @@
-import React, { FormEvent, useState } from 'react';
+import React from 'react';
 import style from './PopupWithForm.module.css';
-import Popup from '@component/ui/popup';
+import Popup from '@component/ui/Popup';
 import { IPopupWithForm } from '../model/types';
-import Button from '@/Components/ui/Button';
-import Input from '@/Components/ui/Input/Index';
+import Button from '@component/ui/Button';
+import Input from '@component/ui/Input/Index';
+import Spinner from '@component/ui/Spinner';
+import { useForm } from '../model/useForm';
 
 export const PopupWithForm: React.FC<IPopupWithForm> = ({
   closeModal,
-  isModalOpen
+  isModalOpen,
+  switchLoadingState,
+  switchSuccessState,
+  showTooltipModal,
+  isLoading
 }) => {
-  const [formData, setFormData] = useState<Record<string, string>>({});
-  const [isFormValid, setIsFormValid] = useState<boolean>(false);
-  const [isNameInputValid, setIsNameInputValid] = useState<boolean>(false);
-  const [isPhoneInputValid, setIsPhoneInputValid] = useState<boolean>(false);
-  const [isEmailInputValid, setIsEmailInputValid] = useState<boolean>(false);
-
-  function handleFormSubmit(evt: FormEvent<HTMLFormElement>) {
-    evt.preventDefault();
-    console.log(formData);
-  }
-
-  function updateFormData(name: string, value: string) {
-    setFormData({ ...formData, [name]: value });
-  }
-
-  React.useEffect(() => {
-    setIsFormValid(isNameInputValid && isPhoneInputValid && isEmailInputValid);
-  }, [isNameInputValid, isPhoneInputValid, isEmailInputValid, isFormValid]);
+  const {
+    formRef,
+    handleFormSubmit,
+    setIsEmailInputValid,
+    setIsNameInputValid,
+    setIsPhoneInputValid,
+    updateFormData,
+    isFormValid
+  } = useForm({ switchLoadingState, switchSuccessState, showTooltipModal });
 
   return (
-    <Popup closeModal={closeModal} isModalOpen={isModalOpen}>
-      <div className={style.wrapper}>
-        <div className={style.textBlock}>
-          <h3 className={style.title}>
-            Запишитесь <br />
-            на приём онлайн
-          </h3>
-          <p className={style.description}>
-            Администратор свяжется с вами через <br />
-            WhatsApp в течение дня и уточнит детали
-          </p>
+    <>
+      <Popup closeModal={closeModal} isModalOpen={isModalOpen} isFullScreen>
+        <div className={style.wrapper}>
+          <div className={style.textBlock}>
+            <h3 className={style.title}>
+              Запишитесь <br />
+              на приём онлайн
+            </h3>
+            <p className={style.description}>
+              Администратор свяжется с вами через <br />
+              WhatsApp в течение дня и уточнит детали
+            </p>
+          </div>
+          <form
+            noValidate
+            className={style.form}
+            onSubmit={handleFormSubmit}
+            ref={formRef}
+          >
+            <Input
+              inputName="name"
+              placeholder="ФИО"
+              type="text"
+              isInputValid={setIsNameInputValid}
+              isFormOpen={isModalOpen}
+              updateFormData={updateFormData}
+            />
+            <Input
+              inputName="phone"
+              placeholder="Номер телефона"
+              type="text"
+              isInputValid={setIsPhoneInputValid}
+              isFormOpen={isModalOpen}
+              updateFormData={updateFormData}
+            />
+            <Input
+              inputName="email"
+              placeholder="Электронная почта"
+              type="text"
+              isInputValid={setIsEmailInputValid}
+              isFormOpen={isModalOpen}
+              updateFormData={updateFormData}
+            />
+            <Button
+              additionalClass={style.button}
+              disabled={!isFormValid}
+              variant="green"
+              text="Записаться"
+            />
+          </form>
         </div>
-        <form noValidate className={style.form} onSubmit={handleFormSubmit}>
-          <Input
-            name="name"
-            placeholder="ФИО"
-            type="text"
-            isInputValid={setIsNameInputValid}
-            isFormOpen={isModalOpen}
-            updateFormData={updateFormData}
-          />
-          <Input
-            name="phone"
-            placeholder="Номер телефона"
-            type="text"
-            isInputValid={setIsPhoneInputValid}
-            isFormOpen={isModalOpen}
-            updateFormData={updateFormData}
-          />
-          <Input
-            name="email"
-            placeholder="Электронная почта"
-            type="text"
-            isInputValid={setIsEmailInputValid}
-            isFormOpen={isModalOpen}
-            updateFormData={updateFormData}
-          />
-          <Button
-            additionalClass={style.button}
-            disabled={!isFormValid}
-            variant="green"
-            text="Записаться"
-          />
-        </form>
-      </div>
-    </Popup>
+        <Spinner isLoading={isLoading} />
+      </Popup>
+    </>
   );
 };
