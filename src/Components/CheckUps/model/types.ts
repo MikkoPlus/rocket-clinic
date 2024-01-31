b58a10227d67b3fa7663935c5d80cc4051266c2f
mikkoplus <sharov.012@gmail.com>
2024-01-31 19:29:24 +0300

    fix(comp): add open modal with form function to checkup slide

--- src/Components/CheckUps/model/types.ts
+++ src/Components/CheckUps/model/types.ts
@@ -1,7 +1,11 @@
-export type CheckUpProps = {
+export interface ICheckUps {
+  openModal: () => void;
+}
+
+export interface ICheckUp extends ICheckUps {
   title: string;
   gender: string;
   checkups: string[];
   oldPrice: number;
   currentPrice: number;
-};
+}
