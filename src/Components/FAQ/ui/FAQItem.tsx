8bcada5a8848f404897e18ddec01b99888446e73
mikkoplus <sharov.012@gmail.com>
2024-01-31 19:28:43 +0300

    fix(bug): fix bug with delay of close accordeon

--- src/Components/FAQ/ui/FAQItem.tsx
+++ src/Components/FAQ/ui/FAQItem.tsx
@@ -11,20 +11,16 @@ export const FAQItem: React.FC<FAQItemProps> = ({ text, title }) => {
 
   return (
     <li className={style.item}>
-      <div className={style.top}>
+      <div className={style.top} onClick={() => setIsOpen(!isOpen)}>
         <h3 className={style.title}>{title}</h3>
-        <div className={style.control} onClick={() => setIsOpen(!isOpen)}>
+        <div className={style.control}>
           <span className={style.line}></span>
           <span className={`${style.line} ${isOpen ? style.open : ''}`}></span>
         </div>
       </div>
       {text.map((textItem, index) => {
         return (
-          <div
-            key={index}
-            aria-expanded={!isOpen}
-            className={`${style.text} ${isOpen ? style.textVisible : ''}`}
-          >
+          <div key={index} aria-expanded={!isOpen} className={style.text}>
             <p className={style.textContent}>{textItem}</p>
           </div>
         );
